e98467d6c2a857538ae5d6ce149839ffc5f2ec46
anikdas400 <anikchandra400@gmail.com>
2023-09-02 07:16:02 +0600

    finished time set

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 const handleCategory = async () => {
   const res = await fetch(" https://openapi.programming-hero.com/api/videos/categories");
   const data = await res.json();
-  console.log(data.data)
+  console.log(data.data.category)
 
   const tabContainer = document.getElementById('tab-container')
   const showId = data.data;
   showId.forEach(category => {
     const div = document.createElement('div')
     div.innerHTML = `
-        <a onclick ="handleLoadItem('${category.category_id}')" class="tab tab-active">${category.category}</a>
+        <a onclick ="handleLoadItem('${category.category_id}')" class="tab tab-active"> ${category.category}</a>
         `
-    console.log(category)
+    // console.log(category)
     tabContainer.appendChild(div)
 
   });
@@ -19,17 +19,34 @@ const handleCategory = async () => {
 const handleLoadItem = async (categoryId) => {
   const res = await fetch(`https://openapi.programming-hero.com/api/videos/category/${categoryId}`)
   const data = await res.json();
-  console.log(data.data)
+  // console.log(data.data)
+  
+  
 
   const cardContainer = document.getElementById('card-container');
+  
   cardContainer.innerHTML = '';
   const cardData = data.data
   cardData.forEach(items => {
-    console.log(items)
+    console.log(items.authors)
+    // console.log(items.others.posted_date)
+    let num = items.others.posted_date
+    
     const div = document.createElement('div')
+    if(num !== ''){
+      var hours = Math.floor(num / 60)
+      var minutes = num % 60
+      
+    }
+
+
     div.innerHTML = `
         <div class="card card-compact h-80 w-auto bg-base-100 shadow-xl">
-          <figure><img class="w-80 h-40 rounded-xl mt-3 " src=${items.thumbnail} alt="ph-tube" /></figure>
+          <figure><img class="w-80 h-40 rounded-xl mt-3 " src=${items?.thumbnail} alt="ph-tube" />
+          <p class="absolute text-gray-100 mt-32 ml-36 bg-black pl-1 pr-1 rounded-lg">${items.others?.posted_date ? `${hours}hrs ${minutes}min ago` : ''}</p>
+
+          </figure>
+          
           <div class="card-body">
 
           <div class="flex">
@@ -39,7 +56,7 @@ const handleLoadItem = async (categoryId) => {
             </div>
           </div>
           <div class ="ml-3">
-            <h3 class ="font-bold text-xl">${items.title}</h3>
+            <h3 class ="font-bold text-xl">${items?.title}</h3>
             <h2 class ="font-medium mt-2 mb-2">${items.authors[0].profile_name} ${items.authors[0]?.verified ? "<img class ='w-5 h-5 ml-2 inline rounded-full' src='./image/verified.png' alt=''></img>" : ""} </h2>
             <p>${items.others.views} views</p>
             
@@ -50,25 +67,30 @@ const handleLoadItem = async (categoryId) => {
           
         </div>
         `
+
+        
+        
     cardContainer.appendChild(div)
+
+  
+
+  
   })
 
 }
 const handleBlog = () => {
 
-//   const blogContainer = document.getElementById('blog-container')
-//   const ol = document.createElement('ol')
-//   ol.innerHTML = `
-//   <li>dgdgdxgvdgvds</li>
-// `
+
 if(onclick="handleBlog()"){
   window.location.href = 'blog.html'
 }
-blogContainer.appendChild(ol)
+
   
 
 }
 
 
 
+
 handleCategory()
+handleLoadItem('1000')
